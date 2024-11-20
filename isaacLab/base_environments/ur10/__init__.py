08c450bdb2eb9ec47358e6657201a017a88fef53
bull-stack <abullenger88@gmail.com>
2024-11-20 12:47:12 +0100

    some update

--- isaacLab/base_environments/ur10/__init__.py
+++ isaacLab/base_environments/ur10/__init__.py
@@ -6,11 +6,15 @@ import gymnasium as gym
 ##
 # Inverse Kinematics - Absolute Pose Control
 ##
+print('Isaac-Bin-Picking-UR10-IK-Abs-v0')
 gym.register(
     id="Isaac-Bin-Picking-UR10-IK-Abs-v0",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": ik_abs_env_cfg.UR10BinPickingEnvCfg_PLAY,
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
